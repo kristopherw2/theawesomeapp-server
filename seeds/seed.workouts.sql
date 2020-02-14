1cff533b81ba50878cb731fd86763e4ebb4cbb31
Carl Ortutay <carlorty@gmail.com>
2020-02-14 13:29:04 -0500

    Adding in changes to master for local db

--- seeds/seed.workouts.sql
+++ seeds/seed.workouts.sql
@@ -1,14 +1,12 @@
 INSERT INTO workouts (workoutname, userid)
 VALUES
+INSERT INTO workouts (workoutname, userid)
+VALUES
 ('Workout1', 1),
 ('Workout2', 1),
-('Workout3', 1),
-('Workout1', 2),
-('Workout2', 2),
 ('Workout3', 2),
-('Workout1', 3),
-('Workout2', 3),
-('Workout3', 3),
-('Workout1', 4),
-('Workout2', 4),
-('Workout3', 4);
+('Workout4', 2),
+('Workout5', 3),
+('Workout6', 3),
+('Workout7', 4),
+('Workout8', 4);
