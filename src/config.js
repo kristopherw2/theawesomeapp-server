99fea6f9cacc17c1397da62a9b7e46c5657df55d
Carl Ortutay <carlorty@gmail.com>
2020-02-27 22:11:59 -0500

    local edits

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://ort_cloud:order66@localhost//fitness-pizza',
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 /*     TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://kristopherwilliams@localhost/fitness-pizza-test' */
 }
 /* {
